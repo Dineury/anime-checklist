77739364c03e9a8b8cf939ce8138ec4fed73b101
Dineury <DineuryLopez1@gmail.com>
2025-12-21 13:33:52 -0500

    feat: update SearchBar component with magnifier icon and improve layout

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ const handleSearch = async (query) => {
 
   return (
     <>
-      <Navbar />
+      <Navbar/>
     
     <div className='max-w-[1920px] mx-auto px-4 p-[2em] text-center justify-self-center ' >
         <div id='Search-bar-div' className='items-center' >  
@@ -43,14 +43,17 @@ const handleSearch = async (query) => {
       {
         animes.map(anime => {
           return (<div id='image_wrapper' key={anime.mal_id} className='w-[201px]  border-2 m-auto bg-linear-to-br from-gray-400 to-gray-800  ' >
-  <div id='anime_container' >
-              <img src={anime.images.jpg.image_url} className='anime_img'/>
-              <p>{anime.title}</p>
-              <select className='options-container' >
+  <div id='anime_container' className='relative' >
+              <div className="three-dots absolute top-0.5 right-2 text-2xl bg-cyan-600 p-0.2 rounded">⋮</div>
+                  <select name='drop-down' className='options-container absolute   opacity-0 cursor-pointer' >
                 <option value="Watching">Watching</option>
                 <option value="completed">completed</option>
                 <option value="Plan to watch">Plan to watch</option>
               </select>
+                <div/>
+            
+              <img src={anime.images.jpg.image_url} className='anime_img'/>
+              <p>{anime.title}</p>
             </div>
           </div>
           )
--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import magnifier from '../Assets/Magnifier.png'
 
 function SearchBar({ onSearch})  {
 const [searchBar, setSearchBar] = useState('')
@@ -14,15 +15,15 @@ const handleSubmit = (e) => {
 
     return (
         <form onSubmit={handleSubmit}  >
-            <div > 
+            <div className="flex" > 
             <input className="m-3 bg-white placeholder-black-500 text-xl pr-2 pl-2 w-64 rounded-2xl"
             
             placeholder="What can I get you today?!" 
             value={searchBar} 
             onChange={handleSearch}
             id="search-input"
-            />
-            
+            />   
+                <img src={magnifier} className="size-9" />
             </div>
         </form>
     )
