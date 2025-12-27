c0c8f54a1fcaa1c77d6673173407b2e1623ee75a
Dineury Lopez <Dineury>
2025-12-27 11:05:15 -0500

    some changes

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -15,15 +15,17 @@ const handleSubmit = (e) => {
 
     return (
         <form onSubmit={handleSubmit}  >
-            <div className="flex" > 
+            <div className="flex justify-center " > 
             <input className="m-3 bg-white placeholder-black-500 text-xl pr-2 pl-2 w-64 rounded-2xl"
             
-            placeholder="What can I get you today?!" 
+            placeholder="What can I get you today?!"
             value={searchBar} 
             onChange={handleSearch}
             id="search-input"
             />   
-                <img src={magnifier} className="size-9" />
+                <img src={magnifier} className=" rounded-10 size-10 drop-shadow-[0_4px_6px_rgba(0,0,0,0.2)] 
+                 mt-2 hover:drop-shadow-[0_4px_6px_rgba(0,0,0,0.5)] transition-transform duration-200 
+                  hover:scale-110" />
             </div>
         </form>
     )
