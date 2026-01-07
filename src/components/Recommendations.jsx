9ad961c183c73b324c2dff3549e2a0a0e64814fd
Dineury <Dineurylopez1@gmail.com>
2026-01-07 17:10:49 -0500

    feat: add tailwind-scrollbar-hide package and update Recommendations component to filter out hentai content

--- src/components/Recommendations.jsx
+++ src/components/Recommendations.jsx
@@ -6,16 +6,38 @@ const [ recommend, setRecommend ] = useState([])
 const handleFetch = async () => {
 try {
     const promises = []
-    for(let i = 0; i < 10; i++) {
+    for(let i = 0; i < 20; i++) {
         promises.push(fetch('https://api.jikan.moe/v4/random/anime')
         .then(res => res.json()))
     }
     const results = await  Promise.all(promises)
-    const animeArr = results.map(r => r.data)
-    const uniqueArr = animeArr.filter((anime, index, self) => {
-        return self.findIndex(a => a.mal_id === anime.mal_id) === index
-    })
-    setRecommend(uniqueArr)
+ const animeArr = results.map(r => r.data)
+
+const safeArr = animeArr.filter(anime => {
+  const isHentaiByRating =
+    anime.rating?.includes("Rx") ||
+    anime.rating?.includes("Hentai")
+
+  const isHentaiByGenre =
+    anime.genres?.some(g => g.mal_id === 12)
+
+  return (
+    !isHentaiByRating &&
+    !isHentaiByGenre &&
+    anime.type !== "Music" &&
+    anime.type !== "PV" &&
+    anime.type !== "CM" &&
+    anime.images?.jpg?.image_url
+  )
+})
+
+console.log(safeArr)
+const uniqueArr = safeArr.filter((anime, index, self) => {
+  return self.findIndex(a => a.mal_id === anime.mal_id) === index
+})
+
+setRecommend(uniqueArr.slice(0, 10))
+
 } catch(err) {
     console.log(err)
 }
@@ -26,11 +48,12 @@ useEffect(() => {
 },[])
 
 return (
-    <div className=" flex flex-wrap" >
+    <div className=" flex flex-row gap-4 overflow-x-auto snap-x snap-mandatory scroll-smooth px-4 no-scrollbar" >
         {
         recommend.map(anime => {
             return(<div  key={anime.mal_id} 
-            className='image_wrapper  w-50.25  border-2 m-auto bg-linear-to-br from-gray-400 to-gray-800  ' >
+            className='image_wrapper snap-center shrink-0 w-52 border-2 bg-linear-to-br from-gray-400
+    to-gray-800' >
 
   <div  className=' anime_container  relative' >
               <div className="three-dots absolute top-0.5 right-2 text-2xl bg-cyan-600 p-0.2 rounded">⋮</div>
@@ -41,7 +64,7 @@ return (
                 <option value="Plan to watch">Plan to watch</option>
               </select>
 
-              <img src={anime.images.jpg.image_url} className='anime_img' alt={anime.title}/>
+              <img src={anime.images.jpg.image_url} className='anime_img size-55' alt={anime.title}/>
               <p>{anime.title}</p>
             </div>
           </div>
